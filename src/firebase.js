a9ee43e2ff1d3df79668a323eac2414848858374
thomastan99 <71714178+thomastan99@users.noreply.github.com>
2022-03-09 22:57:10 +0800

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app"
+const firebaseConfig = {
+    apiKey: "AIzaSyDzbAzD5fSfjrSDmZDUsHaKw-WmzCJYl-E",
+    authDomain: "eduskool-bc389.firebaseapp.com",
+    projectId: "eduskool-bc389",
+    storageBucket: "eduskool-bc389.appspot.com",
+    messagingSenderId: "365762518398",
+    appId: "1:365762518398:web:4e9d45298c446d0012a72f",
+    measurementId: "G-RK44Z77RYE"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig)
+export default firebaseApp;
