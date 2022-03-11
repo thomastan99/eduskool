465c095724e67b7002b5294f674b8e383f5da3c3
chung tak <77869916+ctako@users.noreply.github.com>
2022-03-11 13:38:40 +0800

    Basic set up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<TopBar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="top">
+        <h1>Subject - Topic</h1>
+        <hr>
+    </div>
+</template>
+
+<style scoped>
+.top {
+    overflow: hidden;
+    margin-top: -50px;
+    font-family: 'Varela Round', sans-serif;
+    font-size: 10px;
+}
+
+h1 {
+    font-family: 'Varela Round', sans-serif;
+    text-align: left;  
+}
+</style>
