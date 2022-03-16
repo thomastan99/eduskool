acd879e1199e9433eb060d3d601ba8c4a8ce97d2
Long Ji-An <77440060+LongJiAn99@users.noreply.github.com>
2022-03-16 12:02:00 +0800

    test

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
-import {initializeApp} from "firebase/app"
+import { initializeApp } from "firebase/app"
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDzbAzD5fSfjrSDmZDUsHaKw-WmzCJYl-E",
-    authDomain: "eduskool-bc389.firebaseapp.com",
-    projectId: "eduskool-bc389",
-    storageBucket: "eduskool-bc389.appspot.com",
-    messagingSenderId: "365762518398",
-    appId: "1:365762518398:web:4e9d45298c446d0012a72f",
-    measurementId: "G-RK44Z77RYE"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const firebaseApp = initializeApp(firebaseConfig)
 export default firebaseApp;
---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div style = "text-align:center;">
+      <h1 id = "mainHead"> eduSKOOL </h1>
+      <div id = "firebaseui-auth-container"></div>
+      <div id = "pagecontent">
+          EduSkool is the leading web application in helping secondary school students catch up or overtake their peers with questions
+          up to date with the latest syllabus
+      </div>
+      <h5>coopyright@cpp-2022</h5>
+  </div>
+</template>
+
+<script>
+import firebase from '@/uifire.js'
+import 'firebase/compat/auth'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+export default {
+    name: "Login",
+
+    mounted() {
+        // To call ui instance
+        var ui = firebaseui.auth.AuthUI.getInstance();
+
+        if (!ui) {
+            // need to create instance only once
+            // initialize the firebaseUI widget using firebase
+            ui = new firebaseui.auth.AuthUI(firebase.auth());
+        }
+
+        var uiConfig = {
+            signInSuccessUrl: '/home',
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            ]
+        };
+
+        ui.start("#firebaseui-auth-container", uiConfig)
+    }
+}
+</script>
+
+<style scoped>
+
+#firebaseui-auth-container {
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+#pagecontent {
+    height: 100px;
+    font-size: larger;
+    font-weight: bolder;
+    text-align: center;
+}
+
+#mainHead {
+    text-align: center;
+    text-shadow: 2px 2px grey;
+}
+
+#bg {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+}
+
+h5 {
+    text-align: center;
+    background-color: rgba(12, 170, 136, 0.61)
+}
+</style>
