68f8e4e81cc5cb8a2310d2e9e2d506a8991dbc7b
nasryy <nasryarfan@gmail.com>
2022-03-19 01:01:43 +0800

    edited index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router"
+import TeachersHome from '@/views/TeachersHome.vue'
+import TeachersScoreboard from '@/views/TeachersScoreboard.vue'
+import TeachersCS from '@/views/TeachersCS.vue'
 
 const routes = [
- 
+    {
+        path: '/',
+        name: 'TeachersHome',
+        component: TeachersHome,
+    },
+    {
+        path: '/teachersscoreboard',
+        name: 'TeachersScoreboard',
+        component: TeachersScoreboard,
+    },
+    {
+        path: '/teacherscs',
+        name: 'TeachersCS',
+        component: TeachersCS
+    },
 ]
+
 const router = createRouter({
     history: createWebHistory(),
     routes
