eb5ae59b9318fc15e5c32ccc665f331d5d82e9da
nasryy <nasryarfan@gmail.com>
2022-03-19 00:40:39 +0800

    added basic features

---
+++ src/views/TeachersHome.vue
@@ -0,0 +1,20 @@
+<template>
+<BlueBanner/>
+<LeftPanel/>
+<ClassList/>
+</template>
+
+<script>
+import BlueBanner from '@/components/BlueBanner.vue'
+import LeftPanel from '@/components/LeftPanel.vue'
+import ClassList from '@/components/ClassList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlueBanner,
+    LeftPanel,
+    ClassList
+  }
+}
+</script>
---
+++ src/views/TeachersScoreboard.vue
@@ -0,0 +1,20 @@
+<template>
+<BlueBanner/>
+<LeftPanel/>
+<StudentList/>
+</template>
+
+<script>
+import BlueBanner from '@/components/BlueBanner.vue'
+import LeftPanel from '@/components/LeftPanel.vue'
+import StudentList from '@/components/StudentList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlueBanner,
+    LeftPanel,
+    StudentList
+  }
+}
+</script>
