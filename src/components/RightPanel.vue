ed2dabf0072ad81b47ff26f201b33b377893cd57
thomastan99 <71714178+thomastan99@users.noreply.github.com>
2022-03-20 15:47:17 +0800

    Update RightPanel.vue

--- src/components/RightPanel.vue
+++ src/components/RightPanel.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main">
-        <div id="photo"><img src="../assets/photo3.png" alt="Profile Pic"></div>
+        <div id="photo"><img src="../assets/photo.png" alt="Profile Pic"></div>
         <div class="details">
             <h3><b>Email: </b>prototype@test.com</h3>
             <h3><b>Contact Number: </b>+65 9123 4567</h3>
