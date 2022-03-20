925dbd8d8ade016c39a2ba27697b2e371c2900c6
wchab <weicheng1723@gmail.com>
2022-03-20 15:41:24 +0800

    Homepage
    Homepage

---
+++ src/views/Homepage.vue
@@ -0,0 +1,24 @@
+<template>
+<BlueBanner/>
+<LeftPanel/>
+<Topics/>
+</template>
+
+<script>
+import LeftPanel from '../components/LeftPanel.vue'
+import BlueBanner from '../components/BlueBanner.vue'
+import Topics from '../components/Topics.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LeftPanel,
+    BlueBanner,
+    Topics,
+  }
+}
+</script>
+
+<style>
+
+</style>
